26dad693b2a7b3c27b05a85ef5ef871f0487e448
pyguy12 <wuwarren7360@gmail.com>
2019-07-18 18:43:04 -0500

    refactored inputs to be fields and also changed our default html form into a formik Form component

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // import withFormik and Yup
-import { withFormik } from "formik";
+import { withFormik, Form, Field } from "formik";
 import Yup from "yup";
 
 // Formik injects a lot of props into your component. Here, we're destructuring the value prop.
 // Formik also gives us handleChange which is what we use instead of writing our own onChange handler. You have to have handleChange to make your form writable.
-const App = ({ values, handleChange }) => {
+// We are also given a handleSubmit function we can pass to our onSubmit listeners. However, we do have to write the middleware for it ourselves down in our withFormik function object that's passed into it.
+const App = ({ values }) => {
   return (
-    <div>
-      <input
-        onChange={handleChange}
-        value={values.email}
-        type="email"
-        name="email"
-        placeholder="Email"
-      />
-    </div>
+    // The Form component is provided by Formik and it doesn't need you to pass in handleSubmit to it. It'll use your handleSubmit function as if we passed it in.
+    // Same is true for the Field component, we don't need to pass in the value or onChange handler anymore.
+    <Form>
+      <Field type="email" name="email" placeholder="Email" />
+      <Field type="password" name="password" placeholder="Password" />
+      <label>
+        <Field type="checkbox" name="newsletter" checked={values.newsletter} />
+        Join our newsletter
+      </label>
+      <Field component="select" name="plan">
+        <option value="free">Free</option>
+        <option value="premium">Premium</option>
+      </Field>
+      <button>Submit</button>
+    </Form>
   );
 };
 
@@ -24,16 +31,20 @@ const App = ({ values, handleChange }) => {
 const FormikApp = withFormik({
   // mapPropsToValues maps whats returned in the object from the function to the values property which is then mapped to the props of our original component.
   // mapPropsToValues can also be passed props which in this case we have destructured. The prop we passed in was an email prop in the FormikApp component in our render method below.
-  mapPropsToValues({ email }) {
+  mapPropsToValues({ email, password, newsletter, plan }) {
     return {
       // This 'email' key is the same as the name attribute for our input.
-      email: email || ""
+      email: email || "",
+      password: password || "",
+      newsletter: newsletter || true,
+      plan: plan || "free"
     };
+  },
+  // The first parameter is the values that are returned from the form inputs.
+  handleSubmit(values) {
+    console.log(values);
   }
 })(App);
 
 // We're actually rendering our whatever is returned from the function that's returned from withFormik.
-ReactDOM.render(
-  <FormikApp email="warren@test.io" />,
-  document.querySelector("#root")
-);
+ReactDOM.render(<FormikApp />, document.querySelector("#root"));
